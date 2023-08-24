d6be24c3d3107f1e5a861ea8b9a88880b150e848
missvader <vanepalazon611@gmail.com>
2023-08-24 10:29:23 +0200

    set up project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,48 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        mont: ["var(--font-mont)"],
+        mono: ["var(--font-roboto-mono)"],
+      },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#ED2B2A", // 240,86,199
+        secondary: "#FF0303", // 80,230,217
+      },
+      keyframes: {
+        wave: {
+          "0%": { transform: "rotate(0.0deg)" },
+          "10%": { transform: "rotate(14deg)" },
+          "20%": { transform: "rotate(-8deg)" },
+          "30%": { transform: "rotate(14deg)" },
+          "40%": { transform: "rotate(-4deg)" },
+          "50%": { transform: "rotate(10.0deg)" },
+          "60%": { transform: "rotate(0.0deg)" },
+          "100%": { transform: "rotate(0.0deg)" },
+        },
+        jump: {
+          "0%": { transform: "translateY(0px)" },
+          "100%": { transform: "translateY(20px)" },
+        },
+        sideways: {
+          "0%, 100%": { left: "0", top: "0" },
+          "50%": { left: "100%", top: "0" },
+        },
+        pulse: {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: ".5" },
+        },
+      },
+      animation: {
+        "waving-hand": "wave 6s linear ",
+        "jump-animation": "jump 3s ease infinite",
+        "sideways-animation": "sideways 3s linear infinite",
+        "pulse-animation": "pulse 5s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
     },
   },
